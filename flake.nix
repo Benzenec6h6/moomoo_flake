c80b4938129a12ac3150b7302c802252f7433948
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-01-22 18:27:40 +0900

    Change to OS usage

--- flake.nix
+++ flake.nix
@@ -7,24 +7,28 @@
       pkgs = import nixpkgs { inherit system; };
       sources = pkgs.callPackage ./_sources/generated.nix { };
       
-      # 手元のファイルを使う場合はこちら
-      localDeb = ./moomoo_desktop_16.1.14618_amd64.deb;
-      # nvfetcherで取得したファイルを使う場合は sources.moomoo.src
-      moomoo-deb = localDeb; 
+      moomoo-deb = sources.moomoo.src; 
 
     in {
+      packages.${system} = {
+        default = pkgs.stdenv.mkDerivation {
+          name = "moomoo-desktop-deb";
+          src = moomoo-deb;
+          dontUnpack = true;
+          installPhase = "cp $src $out";
+        };
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [ pkgs.distrobox pkgs.podman ];
 
         # 環境変数として.debのパスを渡す
         shellHook = ''
-          export MOOMOO_DEB_PATH="${moomoo-deb}"
+          export MOOMOO_DEB_PATH="${self.packages.${system}.default}"
           
-          echo "=== Moomoo Deployment Shell ==="
-          echo "1. Run: distrobox create -n moomoo -i ubuntu:22.04"
-          echo "2. Run: distrobox enter moomoo -- sudo apt update"
-          echo "3. Run: distrobox enter moomoo -- sudo apt install -y $MOOMOO_DEB_PATH"
-          echo "4. Run: distrobox enter moomoo -- distrobox-export --app moomoo"
+          echo "=== Moomoo Deployment Shell (nvfetcher version) ==="
+          echo "deb path: $MOOMOO_DEB_PATH"
+          echo "1. distrobox enter moomoo -- sudo apt install -y $MOOMOO_DEB_PATH"
         '';
       };
     };
